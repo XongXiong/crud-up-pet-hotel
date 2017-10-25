00dc73a2589ea4f7bf7b6f38c9fb7de3e57856ea
Xong Xiong <xxiong3153@gmail.com>
2017-10-25 10:47:46 -0500

    append functioning

--- server/routes/pet-router.js
+++ server/routes/pet-router.js
@@ -3,9 +3,31 @@ var router = express.Router();
 var pool = require('../modules/pool.js');
 
 // GET /pet
-
-// GET /owner
-
+router.get('/', function (req, res) {
+    // Attempt to connect to the database
+    pool.connect(function (errorConnectingToDb, db, done) {
+        if (errorConnectingToDb) {
+            // There was an error and no connection was made
+            console.log('Error connecting', errorConnectingToDb);
+            res.sendStatus(500);
+        } else {
+            // We connected to the db!!!!! pool -1
+            //added ordering
+            var queryText = 'SELECT * FROM "owners" o JOIN "pets" p ON o."id" = p."owner_id";';
+            db.query(queryText, function (errorMakingQuery, result) {
+                // We have received an error or result at this point
+                done(); // pool +1
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                    console.log(result.rows);
+                }
+            }); // END QUERY
+        }
+    }); // END POOL
+})
 // POST /owner
 
 // POST /pet
