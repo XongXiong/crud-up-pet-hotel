ef79aa6d72f66db86abebb2ccf03f8f870a9b749
Xong Xiong <xxiong3153@gmail.com>
2017-10-25 10:34:32 -0500

    added append

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -2,4 +2,31 @@ $(document).ready(main);
 
 function main() {
     console.log('JS/jQ working');
+    getPets();
+}
+
+function getPets() {
+    $.ajax({
+        method: 'GET',
+        url: '/pets'
+    }) .done(function (response) {
+        appendPetData(response);
+    }) .fail(function(error) {
+        console.log('We ran into a problem', error);
+    })
+}
+
+function appendPetData(response) {
+    $('#tBody').empty();
+    for (var i = 0; i < response.length; i += 1) {
+        var data = response[i];
+        var $tr = $('<tr></tr>');
+        $tr.append('<td>' + data.owner + '</td>');
+        $tr.append('<td>' + data.name + '</td>');
+        $tr.append('<td>' + data.breed + '</td>');
+        $tr.append('<td>' + data.color + '</td>');
+        $tr.append('<td><button type="button" class="updatebtn">' + data.update + '</button></td>');
+        $tr.append('<td><button type="button" class="deletebtn">' + data.delete + '</button></td>');
+        $tr.append('<td><button type="button" class="checkbtn">' + data.checkStatus + '</button></td>');
+    }
 }
