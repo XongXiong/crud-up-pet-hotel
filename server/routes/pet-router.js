f483510a5e5bc53fd4899e2c5cac781c6e7dda77
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-25 10:13:56 -0500

    routes commented in on router

--- server/routes/pet-router.js
+++ server/routes/pet-router.js
@@ -2,3 +2,18 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pool.js');
 
+// GET /pet
+
+// GET /owner
+
+// POST /owner
+
+// POST /pet
+
+// PUT /in/:id
+
+// PUT /out/:id
+
+// PUT /pet/:id
+
+// DELETE /:id
