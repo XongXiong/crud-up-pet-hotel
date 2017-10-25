37aab14740760e040af1c04f26cc2be06c1cf94c
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-25 10:37:22 -0500

    post owner route in router created

--- server/routes/pet-router.js
+++ server/routes/pet-router.js
@@ -7,7 +7,33 @@ var pool = require('../modules/pool.js');
 // GET /owner
 
 // POST /owner
+// {
+//   firstName:
+//   lastName:
+// }
 
+router.post('/owner',function(req,res){
+  var owner = req.body;
+  pool.connect(function(errorConnectingToDb,db,done){
+    if(errorConnectingToDb){
+      console.log('Error connecting to DB');
+      res.sendStatus(500);
+    } else {
+      var queryText = 'INSERT INTO "owners" ("first_name","last_name") VALUES ($1, $2);';
+      db.query(queryText,[owner.firstName,owner.lastName],function(errorQueryingDb,result) {
+        done();
+        if (errorQueryingDb) {
+          console.log('Error in POST route querying database with');
+          console.log(queryText);
+          res.sendStatus(500);
+        } else {
+          console.log('New todo added:',newTodo);
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+});
 // POST /pet
 
 // PUT /in/:id
