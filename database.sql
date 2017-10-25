e9295c37de967bd573d30871fb18b7f9046d38ab
Hanna Nguyen <ha.nguyenthidan@gmail.com>
2017-10-25 10:46:48 -0500

    Added data for sql tables

--- database.sql
+++ database.sql
@@ -18,3 +18,25 @@ CREATE TABLE "visits" (
 	"check-out_date" DATE,
 	"pet_id" INTEGER REFERENCES "pets"
 );
+
+INSERT INTO "owners" ("first_name", "last_name")
+VALUES ('Aaron', 'Kvarnlov-Leverty'),
+('Laura', 'Goetz'),
+('Xong', 'Xiong'),
+('Hanna', 'Nguyen');
+
+
+INSERT INTO "pets" ("name", "breed", "color", "owner_id")
+VALUES ('Sunny', 'Pitbulls', 'Blue', 1),
+('Winnie', 'Crogi', 'Red', 2),
+('Kitty', 'Australian Sheperd', 'Orange', 3),
+('Panda', 'Malamute', 'White', 4);
+
+
+
+INSERT INTO "visits" ("check-in_date", "check-out_date", "pet_id")
+VALUES ('01/01/2017', '01/30/2017', 1),
+('02/01/2017', '02/28/2017', 2),
+('03/01/2017', '03/30/2017', 3),
+('04/01/2017', '04/30/2017', 4);
+
